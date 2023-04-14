805a1b850cf8aaf0cd2fca1561893d125a3340fe
unknown <albertoi@20143ALBERTOI.ih.gov.mo>
2023-04-14 13:23:09 +0800

    missing update

--- src/routes/yrunnerRoute.js
+++ src/routes/yrunnerRoute.js
@@ -6,7 +6,11 @@ const verifyCmdTextArray = require('../middleware/verifyCmdTextArray')
 const verifyCmdTextInsert = require('../middleware/veryfiCmdTextInsert')
 const { runSQL, runValueSQL, runSelectSQL, runInsertSQLYieldRowID } = require('../yrunner')
 const { handle404 } = require('../middleware/handle404')
+const url = require('url');
 
+/*
+   YRunner 
+*/
 router.get('/runselectsql', verifyPassphrase, verifyCmdText, async (req, res) => {
     const result = await runSelectSQL(req.body.cmdText)
 
@@ -31,6 +35,82 @@ router.post('/runinsertsqlyieldrowid', verifyPassphrase, verifyCmdTextInsert, as
     res.status(result.success ? 200 : 400).json(result)
 })
 
+/*
+   RESTful API
+*/
+// Get all 
+router.get('/:table', verifyPassphrase, async (req, res) => {
+    const query = url.parse(req.url,true).query
+    const cmdText = `select * from ${req.params.table}`
+
+    if (query.mockup)
+        return res.status(200).json({cmdText})
+
+    const result = await runSelectSQL(cmdText)    
+
+    res.status(result.success ? 200 : 400).json({cmdText, ...result})
+})
+
+// Get one 
+router.get('/:table/:key', verifyPassphrase, async (req, res) => {
+    const query = url.parse(req.url,true).query
+    const quote = query.keytype==="string" ? "'" : ""    
+    const cmdText = `select * from ${req.params.table} where ${query.keyname}=${quote}${req.params.key}${quote}`
+
+    if (query.mockup)
+        return res.status(200).json({cmdText})
+
+    const result = await runSelectSQL(cmdText)
+
+    res.status(result.success ? 200 : 400).json({cmdText, ...result})
+})
+
+// Create one
+router.post('/:table', verifyPassphrase, async (req, res) => {
+    const query = url.parse(req.url,true).query
+    let fieldList = ''
+    let valueList = ''
+
+    for (const [key, obj] of Object.entries(req.body)) {
+        if (fieldList!=='') fieldList += ', '
+        if (valueList!=='') valueList += ', '
+
+        fieldList += key 
+        valueList += (obj.type==='string'?"'":"") + obj.value + (obj.type==='string'?"'":"")
+      }
+    const cmdText = `insert into ${req.params.table} (${fieldList}) values(${valueList})`
+    if (query.mockup)
+        return res.status(200).json({cmdText})
+
+    const result = await runSQL([cmdText])
+
+    res.status(result.success ? 200 : 400).json({cmdText, ...result})
+})
+
+// Update one
+
+// Delete one 
+router.delete('/:table/:key', verifyPassphrase, async (req, res) => {
+    const query = url.parse(req.url,true).query
+    const quote = query.keytype==="string" ? "'" : ""    
+    const cmdText = `delete from ${req.params.table} where ${query.keyname}=${quote}${req.params.key}${quote}`
+    
+    if (query.mockup)
+        return res.status(200).json({cmdText})
+
+    const result = await runSQL([cmdText])
+
+    res.status(result.success ? 200 : 400).json({cmdText, ...result})
+})
+
 router.all('/*', handle404)
 
 module.exports = router
+
+/*
+   node.js get url query params from http request
+   https://www.codexpedia.com/node-js/node-js-get-url-query-params-from-http-request/
+
+   Object.entries()
+   https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/entries
+*/
